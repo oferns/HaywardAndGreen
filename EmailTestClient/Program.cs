89a28ed794c8fa0342d8089d9eac21d5453807f1
Ollie Ferns <oferns@gmail.com>
2013-03-21 12:31:56 +0000

    Removed service references (unneccessary) and added simple polling code to the console app

--- EmailTestClient/Program.cs
+++ EmailTestClient/Program.cs
@@ -1,117 +1,138 @@
 ﻿namespace EmailTestClient
 {
     using System;
-    using System.Diagnostics;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Security;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Timers;
     using Microsoft.Exchange.WebServices.Data;
 
     internal class Program
     {
-        static ExchangeService service = Service.ConnectToService(UserDataFromConsole.GetUserData(), new TextTraceListener());
+        private static string ServerFqdn = "https://mail.haywardandgreen.com/EWS/Exchange.asmx";
+        private static ExchangeService service;
+        private static string Username = "tomasgilmartin";
+        private static string Password = "Airwolf96";
+        internal static List<PullSubscription> Subscriptions = new List<PullSubscription>();
 
         private static void Main(string[] args)
         {
+            ServicePointManager.ServerCertificateValidationCallback = CertificateValidationCallBack;
+            Console.WriteLine("Creating service at {0}", ServerFqdn);
+            try
+            {
+                service = new ExchangeService(ExchangeVersion.Exchange2007_SP1)
+                {
+                    Credentials = new WebCredentials(Username, Password, "haywardandgreen.com"),
+                    TraceListener = new TextTraceListener(),
+                    TraceFlags = TraceFlags.All,
+                    TraceEnabled = true,
+                    Url = new Uri(ServerFqdn)
+                };
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Exiting...Error creating service: {0}", exception.Message);
+                Console.ReadLine();
+                return;
+            }
+            Console.WriteLine("Service created!");
 
-            SetStreamingNotifications(service);
+            try
+            {
+                PullSubscription subscription = service.SubscribeToPullNotifications(new FolderId[] { WellKnownFolderName.Inbox }, 1440, "", EventType.Created);
+                Subscriptions.Add(subscription);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Exiting...Error creating subscription: {0}", exception.Message);
+                Console.ReadLine();
+                return;
+            }
 
+            Console.WriteLine("Beginning polling at 30 sec interval");
+
+            var timer = new Timer(30000);
+            timer.Elapsed += (s, e) =>
+                {
+                    Console.WriteLine("Begin Polling for 'Quote Request' in the subject line");
+                    foreach (Item item in Subscriptions.SelectMany(pullSubscription => pullSubscription.GetEvents().ItemEvents.Select(itemEvent => Item.Bind(service, itemEvent.ItemId)).Where(item => item.Subject.Contains("Quote Request"))))
+                    {
+                        Console.WriteLine(item.Body.Text);
+                    }
+                    Console.WriteLine("End Polling");
+                };
             Console.WriteLine("\r\n");
-            Console.WriteLine("Press or select Enter...");
+            Console.WriteLine("Press or select Enter to quit polling...");
             Console.Read();
         }
-
-        static void SetStreamingNotifications(ExchangeService service)
+        
+        private static bool RedirectionUrlValidationCallback(string redirectionUrl)
         {
-            // Subscribe to streaming notifications on the Inbox folder, and listen 
-            // for "NewMail", "Created", and "Deleted" events. 
-            StreamingSubscription streamingsubscription = service.SubscribeToStreamingNotifications(
-                new FolderId[] { WellKnownFolderName.Inbox },
-                EventType.NewMail,
-                EventType.Created,
-                EventType.Deleted);
+            // The default for the validation callback is to reject the URL.
+            bool result = false;
 
-            StreamingSubscriptionConnection connection = new StreamingSubscriptionConnection(service, 1);
+            Uri redirectionUri = new Uri(redirectionUrl);
 
-            connection.AddSubscription(streamingsubscription);
-            // Delegate event handlers. 
-            connection.OnNotificationEvent +=
-                new StreamingSubscriptionConnection.NotificationEventDelegate(OnEvent);
-            connection.OnSubscriptionError +=
-                new StreamingSubscriptionConnection.SubscriptionErrorDelegate(OnError);
-            connection.OnDisconnect +=
-                new StreamingSubscriptionConnection.SubscriptionErrorDelegate(OnDisconnect);
-            connection.Open();
-
-            Console.WriteLine("--------- StreamSubscription event -------");
+            // Validate the contents of the redirection URL. In this simple validation
+            // callback, the redirection URL is considered valid if it is using HTTPS
+            // to encrypt the authentication credentials. 
+            if (redirectionUri.Scheme == "https")
+            {
+                result = true;
+            }
+            return result;
         }
 
-        static private void OnDisconnect(object sender, SubscriptionErrorEventArgs args)
+        private static bool CertificateValidationCallBack(
+            object sender,
+            X509Certificate certificate,
+            X509Chain chain,
+            SslPolicyErrors sslPolicyErrors)
         {
-            // Cast the sender as a StreamingSubscriptionConnection object.           
-            StreamingSubscriptionConnection connection = (StreamingSubscriptionConnection)sender;
-            // Ask the user if they want to reconnect or close the subscription. 
-            ConsoleKeyInfo cki;
-            Console.WriteLine("The connection to the subscription is disconnected.");
-            Console.WriteLine("Do you want to reconnect to the subscription? Y/N");
-            while (true)
+            // If the certificate is a valid, signed certificate, return true.
+            if (sslPolicyErrors == SslPolicyErrors.None)
             {
-                cki = Console.ReadKey(true);
+                return true;
+            }
+
+            // If there are errors in the certificate chain, look at each error to determine the cause.
+            if ((sslPolicyErrors & SslPolicyErrors.RemoteCertificateChainErrors) != 0)
+            {
+                if (chain != null && chain.ChainStatus != null)
                 {
-                    if (cki.Key == ConsoleKey.Y)
+                    foreach (X509ChainStatus status in chain.ChainStatus)
                     {
-                        connection.Open();
-                        Console.WriteLine("Connection open.");
-                        break;
-                    }
-                    else if (cki.Key == ConsoleKey.N)
-                    {
-                        // The ReadKey in the Main() consumes the E. 
-                        Console.WriteLine("\n\nPress E to exit");
-                        break;
+                        if ((certificate.Subject == certificate.Issuer) &&
+                            (status.Status == X509ChainStatusFlags.UntrustedRoot))
+                        {
+                            // Self-signed certificates with an untrusted root are valid. 
+                            continue;
+                        }
+                        else
+                        {
+                            if (status.Status != X509ChainStatusFlags.NoError)
+                            {
+                                // If there are any other errors in the certificate chain, the certificate is invalid,
+                                // so the method returns false.
+                                return false;
+                            }
+                        }
                     }
                 }
+
+                // When processing reaches this line, the only errors in the certificate chain are 
+                // untrusted root errors for self-signed certificates. These certificates are valid
+                // for default Exchange server installations, so return true.
+                return true;
+            }
+            else
+            {
+                // In all other cases, return false.
+                return false;
             }
         }
-
-        static void OnEvent(object sender, NotificationEventArgs args)
-        {
-            StreamingSubscription subscription = args.Subscription;
-
-            // Loop through all item-related events. 
-            foreach (NotificationEvent notification in args.Events)
-            {
-
-                switch (notification.EventType)
-                {
-                    case EventType.NewMail:
-                        Console.WriteLine("\n-------------Mail created:-------------");
-                        break;
-                    case EventType.Created:
-                        Console.WriteLine("\n-------------Item or folder created:-------------");
-                        break;
-                    case EventType.Deleted:
-                        Console.WriteLine("\n-------------Item or folder deleted:-------------");
-                        break;
-                }
-                // Display the notification identifier. 
-                if (notification is ItemEvent)
-                {
-                    // The NotificationEvent for an email message is an ItemEvent. 
-                    ItemEvent itemEvent = (ItemEvent)notification;
-                    Console.WriteLine("\nItemId: " + itemEvent.ItemId.UniqueId);
-                }
-                else
-                {
-                    // The NotificationEvent for a folder is a FolderEvent. 
-                    FolderEvent folderEvent = (FolderEvent)notification;
-                    Console.WriteLine("\nFolderId: " + folderEvent.FolderId.UniqueId);
-                }
-            }
-        }
-        static void OnError(object sender, SubscriptionErrorEventArgs args)
-        {
-            // Handle error conditions. 
-            Exception e = args.Exception;
-            Console.WriteLine("\n-------------Error ---" + e.Message + "-------------");
-        } 
-
     }
 }
